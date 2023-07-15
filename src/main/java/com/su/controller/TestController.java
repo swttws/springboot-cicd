0a283e24a028bc0614b47948318d6ae93c092ce5
swt <swtywmx@via.cool>
2023-07-15 14:39:20 +0800

    初始化

---
+++ src/main/java/com/su/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.su.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author swt
+ * @date 2023/7/15 14:38
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "123";
+    }
+
+}
