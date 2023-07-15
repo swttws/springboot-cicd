356b6fbab350f56f96dacf1dbd97eec4254be250
swt <swtywmx@via.cool>
2023-07-15 13:40:55 +0800

    初始化

---
+++ src/main/java/com/su/Application.java
@@ -0,0 +1,14 @@
+package com.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 21781
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
